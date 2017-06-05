a1a60a1aad402f1fbed38557f12c9ae87c26e517
Vlad Shmyhlo <aion.stu@gmail.com>
2017-06-05 12:17:27 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import configureStore from './store/configureStore';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({ browserHistory });
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(<App store={store} history={history}/>, document.getElementById('root'));
 registerServiceWorker();
