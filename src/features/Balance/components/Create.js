fa8c4800d0c1fe279fdd875722eb14945cb4d028
bezrukavyi <yaroslav555@gmail.com>
2017-07-02 22:34:52 +0300

    Added list of cryptocoins

--- src/features/Balance/components/Create.js
+++ src/features/Balance/components/Create.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Button, Alert } from 'components'
 import { reduxForm } from 'redux-form'
-import { Input, Select } from 'components/fields'
 import currencies from 'utils/currencies'
 import { map } from 'utils'
+
+import { Button, Alert } from 'components'
+import { Input, Select } from 'components/fields'
+import CryptoSelect from './CryptoSelect'
 
 const currencyOptions = map((name) => ({ text: name, value: name }), currencies)
 
 const Create = ({ handleSubmit, error, submitting }) =>
   <form onSubmit={handleSubmit}>
+    <CryptoSelect />
+
     <Select name='name' placeholder='name' label='Name' options={currencyOptions}/>
     <Input type='number' name='amount' placeholder='amount' label='Amount'/>
     <Input type='number' name='invested' placeholder='invested' label='Invested'/>
--- src/features/User/components/SigninForm.js
+++ src/features/User/components/SigninForm.js
@@ -5,7 +5,7 @@ import { Button, Alert } from 'components'
 import { Link } from 'react-router'
 
 const SigninForm = ({ handleSubmit, error, submitting }) =>
-  <form onSubmit={handleSubmit}  className='form__auth'>
+  <form onSubmit={handleSubmit}  className='container form__auth'>
     <h1>Sign In</h1>
 
     <Input type='email' name='email' placeholder='email' label='Email' />
