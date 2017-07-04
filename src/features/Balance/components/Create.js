fdb47f92d09816ff842e2d924022ac56df50a344
bezrukavyi <yaroslav555@gmail.com>
2017-07-04 22:59:40 +0300

    Fix sign up

--- src/features/Balance/components/Create.js
+++ src/features/Balance/components/Create.js
@@ -10,23 +10,24 @@ import CryptoSelect from './CryptoSelect'
 const currencyOptions = map((name) => ({ text: name, value: name }), currencies)
 
 const Create = ({ handleSubmit, error, submitting }) =>
-  <form onSubmit={handleSubmit}>
+  <form className='balance__form' onSubmit={handleSubmit}>
     <CryptoSelect />
 
-    <Select name='name' placeholder='name' label='Name' options={currencyOptions}/>
-    <Input type='number' name='amount' placeholder='amount' label='Amount'/>
-    <Input type='number' name='invested' placeholder='invested' label='Invested'/>
-    <Input type='number' name='profitPercent' placeholder='profit percent' label='Profit percent'/>
+    <div className='balance__field'>
+      <Input type='number' name='amount' placeholder='amount' label='Amount'/>
+      <Input type='number' name='invested' placeholder='invested' label='Invested'/>
+      <Input type='number' name='profitPercent' placeholder='profit percent' label='Profit percent'/>
 
-    {error &&
-      <Alert type='danger'>
-        {error}
-      </Alert>
-    }
+      {error &&
+        <Alert type='danger'>
+          {error}
+        </Alert>
+      }
 
-    <Button type='submit' disabled={submitting}>
-      Create balance
-    </Button>
+      <Button type='submit' disabled={submitting}>
+        Create balance
+      </Button>
+    </div>
   </form>
 
 export default reduxForm()(Create)
