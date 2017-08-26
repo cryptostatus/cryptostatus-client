2c88e0e4dea0b6aaad824b3a423a71d29dd08c17
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-08-26 20:28:35 +0300

    Rediect with message failure omniauth

--- src/utils/urlParams.js
+++ src/utils/urlParams.js
@@ -1,16 +1,30 @@
 import { transform } from 'lodash'
 import queryString from 'query-string'
 
-export const fetchUrlParam = (name, remove) => {
+const encodeData = (text) =>
+    encodeURIComponent(text)
+    .replace(/\-/g, "%2D")
+    .replace(/\_/g, "%5F")
+    .replace(/\./g, "%2E")
+    .replace(/\!/g, "%21")
+    .replace(/\~/g, "%7E")
+    .replace(/\*/g, "%2A")
+    .replace(/\'/g, "%27")
+    .replace(/\(/g, "%28")
+    .replace(/\)/g, "%29")
+    .replace(/%20/g, '+')
+
+export const urlParam = (name, remove) => {
   const value = queryString.parse(window.location.search)[name]
   if (remove) {
-    const newUrl = window.location.href.replace(`${name}=${encodeURIComponent(value)}`, '')
+    const encodeValue = encodeData(value)
+    const newUrl = window.location.href.replace(`${name}=${encodeValue}`, '')
     window.history.pushState(null, null, newUrl.replace(/\?$/, ''))
   }
   return value
 }
 
 const urlParams = (names, remove) =>
-  transform(names, (result, name) => result[name] = fetchUrlParam(name, remove), {})
+  transform(names, (result, name) => result[name] = urlParam(name, remove), {})
 
 export default urlParams
